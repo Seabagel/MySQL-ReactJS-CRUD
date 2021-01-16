ab13164c3d3cecc9a84e1d2b480c862810073b70
Seabagel <hsrt1367tim@gmail.com>
2021-01-16 14:14:16 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,26 +7,32 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
+  host: "localhost",
   user: "root",
-  host: "local",
   password: `password`,
   database: "employee_system",
 });
 
 app.post("/create", (req, res) => {
-  const name = req.body.name;
-  const age = req.body.age;
-  const position = req.body.position;
-  const wage = req.body.wage;
+  const sqlInsert = `INSERT INTO employees (name, age, position, wage) VALUES ('myName', '32', 'someValue', '69');`;
+  db.query(sqlInsert, (err, res) => {
+    res.send("Data sent");
+  });
+});
 
-  console.log(req.body);
+  // const displayInfo = () =>
+  //   console.log(`Added employee: ${name} + ${age} + ${position} + ${wage}`);
 
-  const displayInfo = () =>
-    console.log(`Added employee: ${name} + ${age} + ${position} + ${wage}`);
+  // const name = req.body.name;
+  // const age = req.body.age;
+  // const position = req.body.position;
+  // const wage = req.body.wage;
 
-  db.query(
-    `INSERT INTO employees (name, age, position, wage) VALUES (${name}, ${age}, ${position}, ${wage})`
-  );
+  // console.log(req.body);
+
+  // db.query(
+  //   `INSERT INTO employees (name, age, position, wage) VALUES (${name}, ${age}, ${position}, ${wage})`
+  // );
   // db.query(
   //   "INSERT INTO employees {name, age, position, wage} VALUES (?, ?, ?, ?)",
   //   [name, age, position, wage],
@@ -38,7 +44,7 @@ app.post("/create", (req, res) => {
   //     }
   //   }
   // );
-});
+
 
 app.listen(3001, () => {
   console.log(`server running at port 3001`);
